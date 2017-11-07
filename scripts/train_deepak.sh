93a2c0982567ec3f2fefd216f7710cc08e5b2bf2
Deepak Pathak <deepak.pathak2206@gmail.com>
2017-11-07 01:37:13 -0800

    fix git fiasco

---
+++ scripts/train_deepak.sh
@@ -0,0 +1,14 @@
+gpu=$1
+trunk=$2
+pos_max=$3
+neg_min=$4
+mask_ratio=$5
+
+CUDA_VISIBLE_DEVICES=$gpu python train_sawyer.py --mask_ratio $mask_ratio \
+--trunk $trunk --pos_max $pos_max --neg_min $neg_min \
+--train_set_path_old /home/fred/poke_nlc_training_new \
+--val_set_path /home/fred/poke_nlc_val_new \
+--train_set_path_new /home/fred/sawyer_data_new \
+--tfmodel_path /home/fred/window_seg/models \
+--tfboard_path /home/fred/window_seg/boards \
+--no_scale_pos_scoring
--- train_sawyer.py
+++ train_sawyer.py
@@ -11,14 +11,14 @@ from data_loader import inputs_poking, inputs_sawyer_data
 slim = tf.contrib.slim
 
 
-def gen_name(prefix, ratio, pos_max, neg_min, trunk, decay, lr_factor, background):
-    return '%s_%s_lambda_%.2f_a_%d_b_%d_decay_%.5f_lr_factor%.2f_background_%s' % (prefix, \
-                            trunk, ratio, pos_max, neg_min, decay, lr_factor, background)
+def gen_name(prefix, ratio, pos_max, neg_min, trunk, decay, lr_factor, background, no_scale_pos_scoring):
+    return '%s_%s_lambda_%.2f_a_%d_b_%d_decay_%.5f_lr_factor%.2f_background_%s_no_scale_pos_%s' % (prefix, \
+                            trunk, ratio, pos_max, neg_min, decay, lr_factor, background, no_scale_pos_scoring)
 
 
 def get_lr(timestep, factor = 1):
     # return 1e-3
-    if timestep <= 30000:
+    if timestep <= 20000:
         return 1e-3 * factor
     elif timestep <= 60000:
         return 5e-4 * factor
@@ -29,7 +29,7 @@ def get_lr(timestep, factor = 1):
 def main():
     parser = argparse.ArgumentParser()
 
-    # parser.add_argument('pretrains_path', type=str, help='path of pretrained model')
+    parser.add_argument('--pretrain_path', type=str, default=None, help='path of pretrained model')
     parser.add_argument('--train_set_path_old', type=str, default='/media/icm_data/poke_nlc_training_new')
     parser.add_argument('--val_set_path', type=str, default='/media/icm_data/poke_nlc_val_new')
     parser.add_argument('--tfmodel_path', type=str, default='/home/fred/Desktop/resnet_seg/models')
@@ -42,16 +42,15 @@ def main():
     parser.add_argument('--save_freq', type=int, default=1000)
     parser.add_argument('--gpu_ratio', type=float, default=0.99)
     parser.add_argument('--lr_factor', type=float, default=1)
-    parser.add_argument('--mask_ratio', type=float, default=32)
+    parser.add_argument('--mask_ratio', type=float, default=24)
     parser.add_argument('--pos_max', type=float, default=22)
     parser.add_argument('--neg_min', type=float, default=66)
     parser.add_argument('--weight_decay', type=float, default=5e-4)
     parser.add_argument('--num_itr', type=int, default=200000)
     parser.add_argument('--trunk', type=str, choices=['resnet50', 'resnet18'], default='resnet18')
     parser.add_argument('--add_background', action='store_true')
-
+    parser.add_argument('--no_scale_pos_scoring', action='store_true')
     args = parser.parse_args()
-
 
     train_set_old_names = list([args.train_set_path_old + '/' + l for l in os.listdir(args.train_set_path_old)])
     train_set_new_names = list([args.train_set_path_new + '/' + l for l in os.listdir(args.train_set_path_new)])
@@ -61,21 +60,21 @@ def main():
     val_set_old_names = list([args.val_set_path + '/' + l for l in os.listdir(args.val_set_path)])
 
     train_pos_segment_img_poke, train_pos_segment_mask_poke, train_pos_segment_score_poke, train_pos_segment_background_poke = inputs_poking(train_set_old_names,\
-                                        pos_max=args.pos_max, neg_min=args.neg_min, positive=True, addBg=args.add_background, batch_size=8)
+                                        pos_max=args.pos_max, neg_min=args.neg_min, positive=True, addBg=args.add_background, batch_size=8, do_scale_pos_scoring=not args.no_scale_pos_scoring)
 
     train_pos_scoring_img_poke, train_pos_scoring_mask_poke, train_pos_scoring_score_poke, train_pos_scoring_background_poke = inputs_poking(train_set_old_names, \
                                         pos_max=0.1, neg_min=args.neg_min, positive=True, addBg=args.add_background, batch_size=4)
 
     train_neg_img_poke, train_neg_mask_poke, train_neg_score_poke, train_neg_background_poke = inputs_poking(train_set_old_names,\
                                         pos_max=args.pos_max, neg_min=args.neg_min, positive=False, addBg=args.add_background, batch_size=4)
-    
-    
+
+
     train_pos_segment_img_sawyer, train_pos_segment_mask_sawyer, train_pos_segment_score_sawyer, train_pos_segment_background_sawyer = \
                                     inputs_sawyer_data(train_set_new_pos_names, "positive", args.pos_max, \
                                                                     args.neg_min, batch_size = 24, addBg=args.add_background)
     train_pos_scoring_img_sawyer, train_pos_scoring_mask_sawyer, train_pos_scoring_score_sawyer, train_pos_scoring_background_sawyer = \
                                     inputs_sawyer_data(train_set_new_pos_names, "positive", 0.1, \
-                                                                    args.neg_min, batch_size = 12, addBg=args.add_background)
+                                                                    args.neg_min, batch_size = 12, addBg=args.add_background, do_scale_pos_scoring=not args.no_scale_pos_scoring)
 
     train_neg_img_sawyer, train_neg_mask_sawyer, train_neg_score_sawyer, train_neg_background_sawyer = \
                                     inputs_sawyer_data(train_set_new_neg_names, "negative", args.pos_max, \
@@ -83,7 +82,7 @@ def main():
     train_neg_from_pos_img_sawyer, train_neg_from_pos_mask_sawyer, train_neg_from_pos_score_sawyer, train_neg_from_pos_background_sawyer = \
                                     inputs_sawyer_data(train_set_new_pos_names,"negative_from_positive", \
                                                                     args.pos_max, args.neg_min, batch_size = 6, addBg=args.add_background)
-    
+
 
     train_segment_img = tf.concat([
         train_pos_segment_img_sawyer,
@@ -123,7 +122,7 @@ def main():
         train_neg_background_sawyer,
         train_neg_from_pos_background_sawyer,
     ], 0)
-    
+
 
 
     learning_rate = tf.placeholder(tf.float32, [])
@@ -141,13 +140,17 @@ def main():
         train_pred_mask, _  = build_resnet18_network(train_segment_img, background=train_segment_background, sess=sess, reuse=True, is_training=True, dropout=0.5, add_background=args.add_background)
         sess.run(tf.initialize_all_variables()) # Initialize ResNet params
 
+    if args.pretrain_path:
+        saver = tf.train.Saver()
+        saver.restore(sess, args.pretrain_path)
+
 
     tmp_vars = set(tf.all_variables()) # Trunk variables
 
     train_mask_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=train_pred_mask, labels=train_segment_mask)
     train_label_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=train_pred_score, labels=train_scoring_score)
-    train_pred_label_mean = tf.reduce_mean(tf.cast(tf.argmax(tf.nn.softmax(train_pred_score, dim=-1), 1), tf.float32))
-    train_data_label_mean = tf.reduce_mean(tf.cast(train_scoring_score, tf.float32))
+    # train_pred_label_mean = tf.reduce_mean(tf.cast(tf.argmax(tf.nn.softmax(train_pred_score, dim=-1), 1), tf.float32))
+    # train_data_label_mean = tf.reduce_mean(tf.cast(train_scoring_score, tf.float32))
     # import pdb; pdb.set_trace()
     train_label_acc = tf.reduce_mean(tf.cast(tf.equal(tf.cast(train_scoring_score, tf.int64), tf.argmax(tf.nn.softmax(train_pred_score, dim=-1), 1)), tf.float32))
 
@@ -163,7 +166,7 @@ def main():
 
     sess.run(tf.initialize_variables(set(tf.all_variables()) - tmp_vars))
 
-    model_name = gen_name('train_sgd', args.mask_ratio, args.pos_max, args.neg_min, args.trunk, args.weight_decay, args.lr_factor, args.add_background)
+    model_name = gen_name('train_sgd', args.mask_ratio, args.pos_max, args.neg_min, args.trunk, args.weight_decay, args.lr_factor, args.add_background, args.no_scale_pos_scoring)
 
     summary_writer = tf.summary.FileWriter(args.tfboard_path +'/'+model_name, graph=tf.get_default_graph())
     model_saver = tf.train.Saver()
@@ -171,8 +174,6 @@ def main():
     loss_summ = []
     loss_summ.append(tf.summary.scalar('train/mask_loss', train_mask_loss))
     loss_summ.append(tf.summary.scalar('train/label_loss', train_label_loss))
-    loss_summ.append(tf.summary.scalar('train/pred_label_mean', train_pred_label_mean))
-    loss_summ.append(tf.summary.scalar('train/pred_data_mean', train_data_label_mean))
     loss_summ.append(tf.summary.scalar('train/label_acc', train_label_acc))
     loss_summ.append(tf.summary.scalar('train/total_loss', train_total_loss))
     loss_summ.append(tf.summary.scalar('decay_loss', decay_loss))
@@ -189,7 +190,6 @@ def main():
     train_pos_img_pred_b = tf.unstack(tf.nn.softmax(tf.stack(tf.unstack(train_pred_mask, axis=0), axis=0), dim=-1),axis=-1)[0] * train_pos_img_b
     train_pos_img_pred_viz = tf.stack([train_pos_img_r, train_pos_img_pred_g, train_pos_img_pred_b], axis=-1)
 
-
     img_summ.append(tf.summary.image('train/pos_img', train_pos_img_viz, max_outputs=10))
     img_summ.append(tf.summary.image('train/pos_img_pred', train_pos_img_pred_viz, max_outputs=10))
     img_summ.append(tf.summary.image('train/neg_img_sawyer', train_neg_img_sawyer, max_outputs=10))
@@ -202,7 +202,6 @@ def main():
 
     print ("Finished creating network")
 
-
     for timestep in range(args.num_itr):
         if np.random.random() > 0.5:
             train_opt = train_mask_opt
@@ -214,7 +213,7 @@ def main():
 
             _, train_summary = sess.run(
                 [
-                    train_opt, 
+                    train_opt,
                     loss_summ_op,
                 ], {
                     learning_rate : get_lr(timestep, args.lr_factor)
@@ -224,7 +223,7 @@ def main():
             summary_writer.add_summary(train_summary, timestep)
 
         else:
-            sess.run([train_opt], { learning_rate: get_lr(timestep) })
+            sess.run([train_opt], { learning_rate: get_lr(timestep, args.lr_factor) })
 
         if timestep % args.save_freq == 0:
             model_saver.save(sess, args.tfmodel_path + '/' + model_name, global_step=timestep)
@@ -232,13 +231,6 @@ def main():
 
     coord.request_stop()
     sess.close()
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
